262fa0875643f98457851bbb4452a411dc117e28
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-07-23 10:00:02 -0500

    added template server for new proto services

--- services/server.go
+++ services/server.go
@@ -13,3 +13,39 @@ func (s *Server) Ping(ctx context.Context, in *PingRequest) (*PingResponse, erro
 	log.Printf("Received")
 	return &PingResponse{Name: "Hello "}, nil
 }
+
+// Login ...
+func (s *Server) Login(ctx context.Context, in *LoginRequest) (*LoginResponse, error) {
+	log.Printf("Received")
+	return &LoginResponse{AccessToken: "dd"}, nil
+}
+
+// Register ...
+func (s *Server) Register(ctx context.Context, in *RegisterRequest) (*RegisterResponse, error) {
+	log.Printf("Received")
+	return &RegisterResponse{AccessToken: "dd"}, nil
+}
+
+// AddAccount ...
+func (s *Server) AddAccount(ctx context.Context, in *AccountAddRequest) (*AccountAddResponse, error) {
+	log.Printf("Received")
+	return &AccountAddResponse{}, nil
+}
+
+// GetAccounts ...
+func (s *Server) GetAccounts(ctx context.Context, in *AccountsRequest) (*AccountsResponse, error) {
+	log.Printf("Received")
+	return &AccountsResponse{Accounts: []*AccountSingle{}}, nil
+}
+
+// GetAccount ...
+func (s *Server) GetAccount(ctx context.Context, in *AccountRequest) (*AccountResponse, error) {
+	log.Printf("Received")
+	return &AccountResponse{Account: &Account{}}, nil
+}
+
+// BuyProduct ...
+func (s *Server) BuyProduct(ctx context.Context, in *BuyProductRequest) (*BuyProductResponse, error) {
+	log.Printf("Received")
+	return &BuyProductResponse{Success: true}, nil
+}
