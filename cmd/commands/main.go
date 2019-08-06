9209a60155cf58c982d15ff66b7dfb2a0df7948f
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-05 23:13:24 -0500

    added base commands

--- cmd/commands/main.go
+++ cmd/commands/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"github.com/urfave/cli"
+	"os"
+	"safebox.jerson.dev/api/modules/config"
+)
 
+func init() {
+	err := config.ReadDefault()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "safebox-commands"
+	app.Usage = "comands for safebox API"
+	app.Version = config.Vars.Version
+
+	app.Commands = []cli.Command{
+		{
+			Name:     "send:email:location",
+			Category: "email",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "username, u",
+				},
+			},
+			Action: func(c *cli.Context) error {
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
 }
--- cmd/cron/main.go
+++ cmd/cron/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/jasonlvhit/gocron"
+	"safebox.jerson.dev/api/modules/config"
+)
+
+func init() {
+	err := config.ReadDefault()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 
+	s := gocron.NewScheduler()
+	s.Every(1).Day().At("11:00").Do(sendMails)
+	<-s.Start()
+}
+
+func sendMails() {
+	fmt.Println("I am runnning task.")
 }
