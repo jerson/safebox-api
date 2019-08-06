cb5bfee7336de3fee29c22e99c0faa30270d43d4
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-06 09:18:16 -0500

    added mux for serve grpc and grpc-web

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"net"
 	"net/http"
 	"safebox.jerson.dev/api/models"
 	"safebox.jerson.dev/api/modules/config"
 	"safebox.jerson.dev/api/modules/context"
 	"safebox.jerson.dev/api/modules/db"
 	"safebox.jerson.dev/api/services"
-	"time"
+
+	"github.com/soheilhy/cmux"
 )
 
 func init() {
@@ -46,8 +49,42 @@ func main() {
 	port := fmt.Sprintf(":%d", config.Vars.Server.Port)
 	fmt.Println("running: ", port)
 
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
 	server := grpc.NewServer()
 	services.RegisterServicesServer(server, &services.Server{})
+
+	mux := cmux.New(listener)
+	grpcListener := mux.Match(cmux.HTTP2())
+	grpcWebListener := mux.Match(cmux.HTTP1HeaderFieldPrefix("content-type", "application/grpc-web"))
+	httpListener := mux.Match(cmux.HTTP1())
+
+	group := new(errgroup.Group)
+	group.Go(func() error { return grpcServe(server, grpcListener) })
+	group.Go(func() error { return grpcWebServe(server, grpcWebListener) })
+	group.Go(func() error { return httpServe(httpListener) })
+	group.Go(func() error { return mux.Serve() })
+
+	err = group.Wait()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func httpServe(listen net.Listener) error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		_, _ = res.Write([]byte("hi"))
+	})
+
+	httpServer := &http.Server{Handler: mux}
+
+	return httpServer.Serve(listen)
+}
+func grpcWebServe(server *grpc.Server, listen net.Listener) error {
 
 	wrappedGRPC := grpcweb.WrapServer(server)
 	handler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
@@ -58,16 +95,10 @@ func main() {
 		http.DefaultServeMux.ServeHTTP(resp, req)
 	})
 
-	s := &http.Server{
-		Addr:           port,
-		Handler:        handler,
-		ReadTimeout:    30 * time.Second,
-		WriteTimeout:   30 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+	httpServer := &http.Server{Handler: handler}
+	return httpServer.Serve(listen)
+}
 
-	err := s.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+func grpcServe(server *grpc.Server, listen net.Listener) error {
+	return server.Serve(listen)
 }
