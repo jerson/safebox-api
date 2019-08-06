6fb9592bb32519afd90b67684eef4024e8441783
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-06 13:12:33 -0500

    added plain format

--- modules/commands/email.go
+++ modules/commands/email.go
@@ -13,21 +13,36 @@ import (
 	"time"
 )
 
-var tmpl = template.New("email")
+var tmplHTML = template.New("html")
+var tmplText = template.New("text")
 
 func init() {
 	var err error
-	tmpl, err = tmpl.Parse(`
+	tmplHTML, err = tmplHTML.Parse(`
 <h1>{{.Name}}</h1>
 
 <p>Hello {{.Username}}, we send you your last location:</p>
 
 latitude: {{.Latitude}}
 longitude: {{.Longitude}}
-longitude: {{.Date}}
+date: {{.Date}}
 
 
 <img width="100%" src="https://static-maps.yandex.ru/1.x/?lang=en-US&ll={{.Latitude}},{{.Longitude}}&z=13&l=map&size=600,300&pt={{.Latitude}},{{.Longitude}},vkbkm"  alt="map"/>
+`)
+	if err != nil {
+		panic(err)
+	}
+
+	tmplText, err = tmplText.Parse(`
+
+Hello {{.Username}}, we send you your last location:
+
+latitude: {{.Latitude}}
+longitude: {{.Longitude}}
+date: {{.Date}}
+
+
 `)
 	if err != nil {
 		panic(err)
@@ -53,28 +68,37 @@ func EmailLocation(ctx context.Context, userID int64) error {
 	if err != nil {
 		return err
 	}
-	buf := &bytes.Buffer{}
 
 	log.Info("build template")
-	err = tmpl.Execute(buf, map[string]interface{}{
+
+	params := map[string]interface{}{
 		"Name":      config.Vars.Name,
-		"Usename":   user.Username,
+		"Username":  user.Username,
 		"Latitude":  location.Latitude,
 		"Longitude": location.Longitude,
 		"Date":      location.Date.Format(time.RFC850),
-	})
+	}
+
+	bufHTML := &bytes.Buffer{}
+	err = tmplHTML.Execute(bufHTML, params)
 	if err != nil {
 		return err
 	}
+	htmlContent := bufHTML.String()
 
-	htmlContent := buf.String()
+	bufText := &bytes.Buffer{}
+	err = tmplText.Execute(bufText, params)
+	if err != nil {
+		return err
+	}
+	plainContent := bufText.String()
 
 	log.Info("prepare email")
 	from := mail.NewEmail(config.Vars.Name, config.Vars.SendGrid.From)
 	to := mail.NewEmail(user.Username, user.Email)
 
 	subject := fmt.Sprintf("Last location used in %s", config.Vars.Name)
-	message := mail.NewSingleEmail(from, subject, to, htmlContent, htmlContent)
+	message := mail.NewSingleEmail(from, subject, to, plainContent, htmlContent)
 
 	log.Info("send email")
 	client := sendgrid.NewSendClient(config.Vars.SendGrid.APIKey)
