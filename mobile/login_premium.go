bebead977b2634c1855040fdcbc4265a31ad3d95
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2019-08-12 09:05:02 -0500

    added premium login for mobile

---
+++ mobile/login_premium.go
@@ -0,0 +1,48 @@
+package safebox
+
+import (
+	"context"
+	"errors"
+	"safebox.jerson.dev/api/services"
+)
+
+// LoginPremium ...
+func (s *SafeBox) LoginPremium(username, password string) (*AuthResponse, error) {
+
+	conn, err := s.dial()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := services.NewServicesClient(conn)
+
+	request := &services.LoginRequest{
+		Username: username,
+		Password: password,
+	}
+	response, err := client.Login(context.Background(), request)
+	if err != nil {
+		return nil, err
+	}
+
+	productResponse, _ := client.HasProduct(context.Background(), &services.HasProductRequest{
+		AccessToken: response.AccessToken,
+		Slug:        "wearableaccess",
+	})
+	if productResponse == nil {
+		return nil, errors.New("premium required")
+	}
+
+	s.Session.login(response)
+	s.Session.setPassword(password)
+	return &AuthResponse{
+		AccessToken: response.AccessToken,
+		DateExpire:  response.DateExpire,
+		Date:        response.Date,
+		KeyPair: &KeyPairResponse{
+			PublicKey:  response.KeyPair.PublicKey,
+			PrivateKey: response.KeyPair.PrivateKey,
+		},
+	}, nil
+}
